25f83d8284ca12c70047f7c763a1d34500d351b4
oidex69 <shresthaprince222@gmail.com>
2026-04-01 18:38:18 +0545

    minor fixes

--- profiling/settings.py
+++ profiling/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-48tbb09$e(@*crxo5h)dcdu(ukzbom643a0g)6i6-+uvpc^55q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
