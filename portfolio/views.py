b6c66c8a101f4e67d58a2031f57d02fe5c9956ca
oidex69 <shresthaprince222@gmail.com>
2026-04-01 21:15:34 +0545

    sending mail api

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from profiling import settings
 from .models import Profile   # ✅ add this
 from .models import SkillCategory, WorkExperience, Education, Project, ProjectTechStack, Certification, Journey_Milestone, Interest, Current, Stats
+from django.core.mail import send_mail
 
 def about(request):
     profile = Profile.objects.first()
+    
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        full_message = f"""
+        You have a new message from your portfolio:
+        Name: {name}
+        Email: {email}
+        Message:
+        {message}
+        """
+        send_mail(
+            subject=subject or "No Subject",
+            message=full_message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=["shresthaprince222@gmail.com"],  # your email
+            fail_silently=False,
+        )
+    
     return render(request, 'portfolio/about.html',
                   {
                       'profile' : profile,
--- profiling/settings.py
+++ profiling/settings.py
@@ -128,3 +128,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'shresthaprince222@gmail.com'
+EMAIL_HOST_PASSWORD = 'skbo ltkm fhxe fqzn'  # NOT your normal password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
