0784d3f521916cb7bddd21cef3da73fb7cba79d6
Prince Raj Shrestha <140739573+princeSthaa@users.noreply.github.com>
2026-04-10 21:34:44 +0545

    Update settings.py

--- profiling/settings.py
+++ profiling/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qniux*i4xmy!^9^j8r3i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1','.vercel.app','portfolio-2-pnzp81uhb-shresthaprince222-6364s-projects.vercel.app']
 
 # Application definition
 
